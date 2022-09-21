268a5ed8bdfe66d8216eb362bb91ae1b5420a43f
FoikaKefir <babosdavid8@gmail.com>
2022-09-21 19:17:30 +0300

    Lab 1

--- lab01/main_01.cpp
+++ lab01/main_01.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
 
+using namespace std;
+
+int countBits(int num);
+void feladat1();
+
+bool setBit(unsigned int& num, int order);
+void feladat2();
+
+double avgArray(double array[], int numElements);
+void feladat3();
+
 int main() {
-    std::cout << "Hello, Lab_01" << std::endl;
+    feladat3();
     return 0;
+}
+
+int countBits(int num) {
+    int s = 0;
+    while (num) {
+        s += num & 1;
+        num = num >> 1;
+    }
+    return s;
+}
+
+void feladat1() {
+    for (int i = 0; i <= 127; ++i) {
+        cout << i << " " << countBits(i) << endl;
+    }
+}
+
+bool setBit(unsigned int &num, int order) {
+    if (order < 0 || order > 31)
+        return false;
+
+    num = num | (1 << order);
+    return true;
+}
+
+void feladat2() {
+    for (int i = 0; i < 35; ++i) {
+        unsigned int n = 0;
+        if (setBit(n, i)) {
+            cout << i << " ---> " << n << endl;
+        } else {
+            cout << "Impossible operation!" << endl;
+        }
+    }
+}
+
+double avgArray(double array[], int numElements) {
+    double avg = 0;
+    for (int i = 0; i < numElements; ++i) {
+        avg += array[i];
+    }
+    return avg / (double) numElements;
+}
+
+void feladat3() {
+    double array[] = {1, 2, 3, 4, 5};
+    int n = 5;
+    cout << avgArray(array, n);
 }
