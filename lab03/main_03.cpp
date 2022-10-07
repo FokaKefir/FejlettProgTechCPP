3f167e2a6e6e6e24bc0bc62fb73d5986d604b464
david <babosdavid8@gmail.com>
2022-10-07 20:17:15 +0300

    Lab 3

--- lab03/main_03.cpp
+++ lab03/main_03.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
 #include "List.h"
-
+#include "Stack.h"
 using namespace std;
 
 int main() {
-    List lst = List();
-    lst.insertFirst(1);
-    lst.insertFirst(2);
-    cout << lst.size() << endl;
-    lst.insertFirst(3);
-    lst.print();
-    lst.removeFirst();
-    cout << lst.size() << endl;
-    lst.remove(2, List::DeleteFlag::LESS);
-    lst.print();
-    cout << lst.size() << endl;
+    List list = List();
+    list.insertFirst(1);
+    list.insertFirst(2);
+    cout << "List size " << list.size() << endl;
+    list.insertFirst(3);
+    list.print();
+    list.removeFirst();
+    cout << "List size " << list.size() << endl;
+    list.remove(2, List::DeleteFlag::LESS);
+    list.print();
+    cout << "List size " << list.size() << endl << endl;
+
+    Stack stack = Stack();
+    stack.push(1);
+    stack.push(2);
+    cout << (stack.isEmpty() == 1 ? "Stack is empty" : "Stack is not empty") << endl;
+    cout << stack.pop() << endl;
+    cout << (stack.isEmpty() == 1 ? "Stack is empty" : "Stack is not empty") << endl;
+    cout << stack.pop() << endl;
+    cout << (stack.isEmpty() == 1 ? "Stack is empty" : "Stack is not empty") << endl;
+    try {
+        cout << stack.pop() << endl;
+    } catch (exception ex) {
+        cout << ex.what();
+    }
     return 0;
 }
